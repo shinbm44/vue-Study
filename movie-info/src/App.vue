69e12427fdcee6489e1534e63b68a1c5b7ec4cc5
shinbm44 <78016592+shinbm44@users.noreply.github.com>
2024-06-18 14:44:50 +0900

    Feat: 이미지 및 레이아웃 정리

--- movie-info/src/App.vue
+++ movie-info/src/App.vue
@@ -2,12 +2,17 @@
   <h1>영화 정보</h1>
   
   <div v-for="(item, i) in data" :key="i">
+    <figure>
+      <img :src="`${this.data[i].imgUrl}`" :alt="item.title">
+    </figure>
+    <div class =:info></div>
     <h3 class="bg-yellow" >{{ item.title }} </h3>
     <p>개봉: {{ item.year }}</p>
     <p>장르: {{ item.category  }} </p>
 
     <button @:click="increaseLike(i)" class="button">좋아요</button> <span>{{ item.like }}</span>
   </div>
+ 
 
 </template>
 
@@ -26,22 +31,25 @@
     
         data : [
           {
-            title: "노랑",
+            title: "노량",
             year: 2023,
             category: "액션, 드라마",
             like :0,
+            imgUrl: "public/노량.jpg"
           },
           {
             title: "아쿠아맨과 로스트 킹덤",
             year: 2023,
             category: "액션, 판타지, 어드벤처",
             like :0,
+            imgUrl: "public/아쿠아맨.jpg"
           },
           {
             title: "3월의 휴가",
             year: 2023,
             category: "판타지, 드라마 ",
             like :0,
+            imgUrl: "public/3일의휴가.jpg"
           }
         ]
 
@@ -60,6 +68,72 @@
     background: gray;
     padding-right: 15px;
     padding-left: 15px;
-
   }
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+body {
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1,
+h2,
+h3 {
+  margin-bottom: 1rem;
+}
+
+p {
+  margin-bottom: 0.5rem;
+}
+
+button {
+  margin-right: 10px;
+  margin-top: 1rem;
+}
+
+.item {
+  width: 100%;
+  border: 1px solid #ccc;
+  display: flex;
+  margin-bottom: 20px;
+  padding: 1rem;
+}
+
+.item figure {
+  width: 30%;
+  margin-right: 1rem;
+}
+
+.item img {
+  width: 100%;
+}
+
+.item .info {
+  width: 100%;
+}
+
+.modal {
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner {
+  background: #fff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
+}
 </style>
+
